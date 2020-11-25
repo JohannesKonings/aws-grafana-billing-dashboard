2a13c28f431d05f060140c3573ca34d550e6ff3d
Johannes Konings <johannes.konings@outlook.com>
2020-11-25 16:06:09 +0100

    terraform variables updted

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "grafana_url" {}
 variable "grafana_api_key" {}
-variable "access_key" {}
-variable "secret_key" {}
+variable "access_key_grafana" {}
+variable "secret_key_grafana" {}
+variable "region" {}
